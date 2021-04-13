390a35f6c126331ba55f23943a1b1e81753c60de
mark-root <mroot@intertech.com>
2021-04-12 20:04:02 -0500

    remove old calculator components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
-import { EnergyEquivalencyComponent } from './energy-equivalency/energy-equivalency.component';
-import { EnergyEquivalencyHelpComponent } from './energy-equivalency/energy-equivalency-help/energy-equivalency-help.component';
-import { FuelFormComponent } from './energy-equivalency/fuel-form/fuel-form.component';
-import { ElectricityFormComponent } from './energy-equivalency/electricity-form/electricity-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CurrentEquipmentFormComponent } from './current-equipment-form/current-equipment-form.component';
 import { ElectricalEquipmentFormComponent } from './electrical-equipment-form/electrical-equipment-form.component';
 import { ResultsComponent } from './results/results.component';
 import { HelpComponent } from './help/help.component';
 import { OperatingHoursComponent } from './operating-hours/operating-hours.component';
-import { NoopAnimationPlayer } from '@angular/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    EnergyEquivalencyComponent,
-    EnergyEquivalencyHelpComponent,
-    FuelFormComponent,
-    ElectricityFormComponent,
     CurrentEquipmentFormComponent,
     ElectricalEquipmentFormComponent,
     ResultsComponent,
